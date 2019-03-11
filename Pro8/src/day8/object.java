e9b5d2ccd9ab31df7ff76e7e5b6eef6a34ae9676
kanshan1 <1260770348@qq.com>
2019-03-11 11:23:22 +0800

    第二次提交

---
+++ Pro8/src/day8/object.java
@@ -0,0 +1,33 @@
+package day8;
+
+public class object {
+	public static void main(String[] args) throws Exception {
+//		String a="Jeff";
+//		System.out.println(a.equals("tom"));
+//		tom cat=new tom();
+//		jerry laoshu=new jerry();
+//		System.out.println(cat.equals(laoshu));
+		div A=new div();
+			System.out.println(A.shout(3, 0));
+	}
+}
+class div{
+	double shout(int a,int b) throws Exception{
+		int c=a/b;
+		return c;
+	}
+}
+
+//class tom{
+//	String name = "tom";
+//	void name(){
+//		System.out.println("你好，我叫" + name);
+//	}
+//}
+//
+//class jerry{
+//	String name = "jerry";
+//	void name(){
+//		System.out.println("你好，我叫" + name);
+//	}
+//}
